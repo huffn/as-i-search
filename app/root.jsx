5a6b6065dcfcc0f73d1f3c41d26011cafc93dfd8
Nathaniel Huff <huff@familysearch.org>
2022-10-10 15:05:14 -0600

    Adding root route

--- app/root.jsx
+++ app/root.jsx
@@ -1,19 +1,38 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
+import  prisma from '~/lib/db.server'
+import mainCss from '~/css/root.css'
+
+export const loader = async () => {
+  const data = {
+    volumes: await prisma.volumes.findMany(),
+  }
+  return data
+}
 
 export const meta = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "As I Search",
   viewport: "width=device-width,initial-scale=1",
 });
 
+export function links() {
+  return [
+    { rel: "stylesheet", href: mainCss }
+  ]
+}
+
 export default function App() {
+  const { volumes } = useLoaderData()
   return (
     <html lang="en">
       <head>
@@ -21,7 +40,23 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <input type="checkbox" name="mobile-toggle" id="mobile-toggle"/>
+        <div className="mobile-wrapper">
+          <div className="wrapper">
+            <nav>
+              <Link to="/"><h1>As I Search</h1></Link>
+              {volumes.map((volume) => (
+                <NavLink to={volume.volume_lds_url} key={volume.volume_lds_url}>
+                  {volume.volume_long_title}
+                </NavLink>
+              ))}
+              <label htmlFor="mobile-toggle">☰</label>
+            </nav>
+            <div className="main">
+              <Outlet />
+            </div>
+          </div>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,30 @@
+import homeCss from '~/css/home.css'
+
+export function links() {
+  return [
+    { rel: "stylesheet", href: homeCss }
+  ]
+}
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <>
+      <p>{`As I search the holy scriptures,
+Loving Father of mankind,
+May my heart be blessed with wisdom,
+And may knowledge fill my mind.`}</p>
+      <p>{`As I search the holy scriptures,
+Touch my spirit, Lord, I pray.
+May life's myst'ries be unfolded
+As I study day by day.`}</p>
+      <p>{`As I search the holy scriptures,
+May thy mercy be revealed.
+Soothe my troubled heart and spirit;
+May my unseen wounds be healed.`}</p>
+      <p>{`As I search the holy scriptures,
+Help me ponder and obey.
+In thy word is life eternal;
+May thy light show me the way.`}</p>
+    </>
+  )
 }
